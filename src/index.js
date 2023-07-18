5613915ac9bb705724386f97768d23397a356c6f
hashaam <hashaamkhan4247@gmail.com>
2023-07-18 20:42:40 +0500

    initial commit, created register and login routes

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const userRoute = require("./routes/users");
+
+const app = express();
+
+app.use(morgan("tiny"));
+
+const corsConfig = {
+  credentials: true,
+  origin: true,
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+};
+app.use(cors(corsConfig));
+app.use(express.json());
+
+// Routes //
+app.use("/auth", userRoute);
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connected.");
+    app.listen(3000, () => {
+      console.log("server started");
+    });
+  })
+  .catch((err) => console.log(err));
