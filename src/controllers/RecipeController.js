7e4fc6837a85e567dc31ad8bba2fd7ad40347deb
hashaam <hashaamkhan4247@gmail.com>
2023-07-20 23:25:11 +0500

    added saved recipes controller

--- src/controllers/RecipeController.js
+++ src/controllers/RecipeController.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const RecipeModel = require("../models/Recipes");
+const UserModel = require("../models/Users");
 
 const getAllRecipes = async (req, res) => {
   try {
@@ -21,7 +22,33 @@ const createRecipe = async (req, res) => {
   }
 };
 
+const saveRecipe = async (req, res) => {
+  try {
+    const recipe = await RecipeModel.findById(req?.params?._id);
+    const user = await UserModel.findById(req?.user?._id);
+
+    if (!recipe) {
+      return res.status(404).json({ message: "Recipe not found" });
+    }
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    user.savedRecipes.push(recipe);
+
+    await user.save();
+
+    return res
+      .status(200)
+      .json({ message: "Recipe saved", savedRecipes: user.savedRecipes });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
 module.exports = {
   getAllRecipes,
   createRecipe,
+  saveRecipe,
 };
--- src/routes/recipes.js
+++ src/routes/recipes.js
@@ -2,12 +2,38 @@ const express = require("express");
 const {
   getAllRecipes,
   createRecipe,
+  saveRecipe,
 } = require("../controllers/RecipeController");
+const UserModel = require("../models/Users");
+const RecipeModel = require("../models/Recipes");
 
 const router = express.Router();
 
 router.get("/", getAllRecipes);
 
 router.post("/", createRecipe);
+
+router.put("/", saveRecipe);
+
+router.get("/savedRecipes/ids", async (req, res) => {
+  try {
+    const user = await UserModel.findById(req?.user?._id);
+    return res.status(200).json({ savedRecipes: user?.savedRecipes });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+});
+
+router.get("/savedRecipes", async (req, res) => {
+  try {
+    const user = await UserModel.findById(req?.user?._id);
+    const savedRecipes = await RecipeModel.find({
+      _id: { $in: user?.savedRecipes },
+    });
+    return res.status(200).json({ savedRecipes });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+});
 
 module.exports = router;
